ba1bd16204f2932932dd059d40e37e6b71ce8b14
Dennis Kraft <dennis.kraft@visualvest.de>
2023-09-14 16:10:02 +0200

    linting

--- src/imageHandling/imageUtils.ts
+++ src/imageHandling/imageUtils.ts
@@ -1,26 +1,26 @@
-import { existsSync, mkdirSync } from 'fs';
-import { type Request } from 'express';
-import * as paths from '../paths';
-import resize from '../utils/sharp';
+import { existsSync, mkdirSync } from 'fs'
+import { type Request } from 'express'
+import * as paths from '../paths'
+import resize from '../utils/sharp'
 
 class ImageUtils {
-  async resizeImage(req: Request): Promise<string> {
-    const { filename, width, height } = req.query;
-    const w = Number(width);
-    const h = Number(height);
-    const image = (filename as string) + '.jpg';
-    const newImage = (filename as string) + `_${w}_${h}.jpg`;
-    const imagePath = paths.images + '/' + image;
-    const storagePath = paths.storage;
+  async resizeImage (req: Request): Promise<string> {
+    const { filename, width, height } = req.query
+    const w = Number(width)
+    const h = Number(height)
+    const image = (filename as string) + '.jpg'
+    const newImage = (filename as string) + `_${w}_${h}.jpg`
+    const imagePath = paths.images + '/' + image
+    const storagePath = paths.storage
     if (!existsSync(storagePath)) {
-      mkdirSync(storagePath);
+      mkdirSync(storagePath)
     }
-    const newImagePath: string = paths.storage + '/' + newImage;
+    const newImagePath: string = paths.storage + '/' + newImage
 
-    await resize(imagePath, w, h, newImagePath);
-    return newImagePath;
+    await resize(imagePath, w, h, newImagePath)
+    return newImagePath
   }
 }
 
-const imageUtils = new ImageUtils();
-export default imageUtils;
+const imageUtils = new ImageUtils()
+export default imageUtils
